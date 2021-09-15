ab75347ec096a5ea26c518ca10c48e5131bfd7b6
W-Sikora <242612@student.pwr.edu.pl>
2021-09-15 15:43:31 +0200

    updated validator * separated exceptions

--- src/main/java/util/Validator.java
+++ src/main/java/util/Validator.java
@@ -9,9 +9,13 @@ public class Validator {
     private static final int MIN_LIST_SIZE = 1;
     private static final int MAX_LIST_SIZE = 5;
     private static final String VALID_CURRENCY_FORMAT = "\\d+\\.\\d{2}";
-    private static final String INVALID_LIST_SIZE_MESSAGE = "ProductList should contain from %d to %d items";
-    private static final String INVALID_CURRENCY_VALUE_MESSAGE = "Currency value cannot be null and must be a positive number following the format ###.##";
-    private static final String INVALID_NAME_MESSAGE = "Product name cannot be null, empty and start with whitespace characters";
+    private static final String INVALID_LIST_SIZE_MESSAGE = String.format("ProductList should contain from %d to %d items", MIN_LIST_SIZE, MAX_LIST_SIZE);
+    private static final String NULL_CURRENCY_VALUE_MESSAGE = "Currency value cannot be null";
+    private static final String NEGATIVE_NUMBER_CURRENCY_VALUE_MESSAGE = "Currency value must be a positive number";
+    private static final String INVALID_FORMAT_CURRENCY_VALUE_MESSAGE = "Currency value must be in `###.##` format";
+    private static final String NULL_NAME_MESSAGE = "Product name cannot be null";
+    private static final String EMPTY_NAME_MESSAGE = "Product name cannot be empty";
+    private static final String BLANK_NAME_MESSAGE = "Product name cannot contains only white space";
 
     private Validator() {
     }
@@ -19,25 +23,37 @@ public class Validator {
     public static void validateProducts(List<Product> products) {
         int size = products.size();
         if (size < MIN_LIST_SIZE || size > MAX_LIST_SIZE) {
-            throw new IllegalArgumentException(String.format(INVALID_LIST_SIZE_MESSAGE, MIN_LIST_SIZE, MAX_LIST_SIZE));
+            throw new IllegalArgumentException(INVALID_LIST_SIZE_MESSAGE);
         }
         products.forEach(Validator::validateProduct);
     }
 
-    public static void validateCurrency(BigDecimal price) {
-        if (price == null || !isCurrencyFormatValid(price) || price.signum() > -1) {
-            throw new IllegalArgumentException(INVALID_CURRENCY_VALUE_MESSAGE);
+    public static void validateCurrencyValue(BigDecimal price) {
+        if (price == null) {
+            throw new IllegalArgumentException(NULL_CURRENCY_VALUE_MESSAGE);
+        }
+        if (price.signum() < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_CURRENCY_VALUE_MESSAGE);
+        }
+        if (!isCurrencyFormatValid(price)) {
+            throw new IllegalArgumentException(INVALID_FORMAT_CURRENCY_VALUE_MESSAGE);
         }
     }
 
     private static void validateProduct(Product product) {
         validateName(product.getName());
-        validateCurrency(product.getPrice());
+        validateCurrencyValue(product.getPrice());
     }
 
     private static void validateName(String name) {
-        if (name == null || name.isEmpty() || name.isBlank()) {
-            throw new IllegalArgumentException(INVALID_NAME_MESSAGE);
+        if (name == null) {
+            throw new IllegalArgumentException(NULL_NAME_MESSAGE);
+        }
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_NAME_MESSAGE);
+        }
+        if (name.isBlank()) {
+            throw new IllegalArgumentException(BLANK_NAME_MESSAGE);
         }
     }
 
